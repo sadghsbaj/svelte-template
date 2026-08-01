7d92c902e741954ecf518d15af05ffa4be1ecd76
sadghsbaj <colinwsnm@gmail.com>
2026-08-01 17:36:42 +0200

    feat: restrict hover utilities to devices with hover support

--- uno.config.ts
+++ uno.config.ts
@@ -142,4 +142,15 @@ export default defineConfig({
                 `,
         },
     ],
+
+    variants: [
+        (matcher) => {
+            if (!matcher.startsWith("hover:")) return matcher;
+            return {
+                matcher: matcher.slice(6),
+                parent: "@media (hover: hover)",
+                selector: (s) => `${s}:hover`,
+            };
+        },
+    ],
 });

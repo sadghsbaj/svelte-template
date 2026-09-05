d6ad7a5a5a531fd8df8a54349b1844d723dd0a2c
sadghsbaj <colinwsnm@gmail.com>
2026-09-05 17:55:39 +0200

    feat(button): apply text-trim utility to base button styles

--- src/lib/shared/components/_system/button/button.styles.ts
+++ src/lib/shared/components/_system/button/button.styles.ts
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from "$utils";
 
 export const buttonStyles = cva({
     base: {
-        layout: "inline-flex justify-center items-center shrink-0 w-fit select-none",
+        layout: "text-trim inline-flex justify-center items-center shrink-0 w-fit select-none",
         typography: "font-600 leading-normal",
         frame: "rounded-full squircle-smooth",
         interaction: "cursor-pointer active:scale-97",
